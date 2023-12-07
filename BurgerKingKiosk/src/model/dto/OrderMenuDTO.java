4bba6b43d34cf5286ae3d87a7f8a777a7a445a8e
suucong <sophia021210@naver.com>
2023-12-07 23:11:46 +0900

    feat: Add ingredientList to OrderMenuDTO (#37)

--- BurgerKingKiosk/src/model/dto/OrderMenuDTO.java
+++ BurgerKingKiosk/src/model/dto/OrderMenuDTO.java
@@ -1,19 +1,23 @@
 package model.dto;
 
+import java.util.ArrayList;
+
 public class OrderMenuDTO {
 	private int quantity;	// 주문 메뉴 수량
 	private String side;
 	private String drink;
 	private int menuId;		// 메뉴 번호(FK)
 	private int orderId;	// 주문 번호(FK)
+	private ArrayList<String> ingredientList = new ArrayList<>();	// 재료 추가 배열
 	
 	// constructor
-	public OrderMenuDTO(int quantity, String side, String drink, int menuId, int orderId) {
+	public OrderMenuDTO(int quantity, String side, String drink, int menuId, int orderId, ArrayList<String> ingredientList) {
 		this.quantity = quantity;
 		this.side = side;
 		this.drink = drink;
 		this.menuId = menuId;
 		this.orderId = orderId;
+		this.ingredientList = ingredientList;
 	}
 	
 	// getter
@@ -37,6 +41,10 @@ public class OrderMenuDTO {
 		return orderId;
 	}
 	
+	public ArrayList<String> getIngredientList() {
+		return ingredientList;
+	}
+	
 	// setter
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
@@ -56,5 +64,9 @@ public class OrderMenuDTO {
 	
 	public void setOrderId(int orderId) {
 		this.orderId = orderId;
+	}
+	
+	public void setIngredientList(ArrayList<String> ingredientList) {
+		this.ingredientList = ingredientList;
 	}
 }
