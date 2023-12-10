24fa3afc651f3219a9006871a1deeea528d49b70
suucong <sophia021210@naver.com>
2023-12-10 21:46:57 +0900

    feat: Modify MenuDAO to retrieve menu information by type and create MenuByTypeDTO Objects (#40)

--- BurgerKingKiosk/src/model/dao/MenuDAO.java
+++ BurgerKingKiosk/src/model/dao/MenuDAO.java
@@ -2,37 +2,78 @@ package model.dao;
 
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
+import jdbc.MysqlJdbc;
+import model.dto.MenuByTypeDTO;
+import model.vo.MenuTypeVO;
+import model.vo.MenuVO;
 
 public class MenuDAO {
-	// type_id를 기준으로 타입 이름, 메뉴 이름, 메뉴 가격을 가져오는 메서드
-	public static Map<Object[], List<Object[]>> getMenuInfoByTypeIdWithJoin(Connection connection) throws Exception {
-	    String query = "SELECT m.type_id, t.type_name, m.menu_name, m.menu_price FROM Menu m " +
-	            "JOIN Type t ON m.type_id = t.type_id " +
-	            "WHERE m.menu_isPossible = 1";
-	    try (PreparedStatement pstmt = connection.prepareStatement(query)) {
-	        ResultSet rs = pstmt.executeQuery();
+	// Type별로 isVisible이 1인 Menu 정보를 가져와서 MenuByTypeDTO로 묶어 반환하는 메서드(실질적으로 BurgerKingMain에서 이용할 메서드)
+    public static List<MenuByTypeDTO> getMenuByTypes() throws Exception {
+        List<MenuByTypeDTO> menuByTypeDTOs = new ArrayList<>();
 
-	        Map<Object[], List<Object[]>> menuInfoMap = new HashMap<>();
-	        while (rs.next()) {
-	            int typeId = rs.getInt("type_id");
-	            String typeName = rs.getString("type_name");
-	            String menuName = rs.getString("menu_name");
-	            int menuPrice = rs.getInt("menu_price");
+        // 모든 Type 정보 가져오기
+        List<MenuTypeVO> allTypes = getAllTypes();
 
-	            // 복합키 생성
-	            Object[] key = {typeId, typeName};
+        // 각 Type에 대해 isVisible이 1인 Menu 정보 가져오기
+        for (MenuTypeVO type : allTypes) {
+            ArrayList<MenuVO> menuVos = getMenuInfoByType(type.getId());
 
-	            // Object 배열에 데이터 저장
-	            Object[] menuInfo = {menuName, menuPrice};
+            // MenuByTypeDTO 생성 및 추가
+            MenuByTypeDTO menuByTypeDTO = new MenuByTypeDTO(type, menuVos);
+            menuByTypeDTOs.add(menuByTypeDTO);
+        }
 
-	            // Map에 추가
-	            menuInfoMap.computeIfAbsent(key, k -> new ArrayList<>()).add(menuInfo);
-	        }
+        return menuByTypeDTOs;
+    }
+    
+    // 모든 Type 정보 가져오기
+    private static List<MenuTypeVO> getAllTypes() throws Exception {
+        List<MenuTypeVO> allTypes = new ArrayList<>();
 
-	        return menuInfoMap;
-	    }
-	}
+        String query = "SELECT * FROM Type";
+
+        try (Connection connection = DriverManager.getConnection(MysqlJdbc.URL, MysqlJdbc.USER, MysqlJdbc.PASSWORD);
+        		PreparedStatement pstmt = connection.prepareStatement(query)) {
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                int typeId = rs.getInt("type_id");
+                String typeName = rs.getString("type_name");
+
+                MenuTypeVO type = new MenuTypeVO(typeId, typeName);
+                allTypes.add(type);
+            }
+        }
+
+        return allTypes;
+    }
+    
+    // Type별 isVisible이 1인 Menu 정보 가져오기
+    private static ArrayList<MenuVO> getMenuInfoByType(int typeId) throws Exception {
+        ArrayList<MenuVO> menuVos = new ArrayList<>();
+
+        String query = "SELECT * FROM Menu WHERE type_id = ? AND menu_isPossible = 1";
+
+        try (Connection connection = DriverManager.getConnection(MysqlJdbc.URL, MysqlJdbc.USER, MysqlJdbc.PASSWORD);
+        		PreparedStatement pstmt = connection.prepareStatement(query)) {
+            pstmt.setInt(1, typeId);
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                int menuId = rs.getInt("menu_id");
+                String menuName = rs.getString("menu_name");
+                long menuPrice = rs.getLong("menu_price");
+                int menuIsPossible = rs.getInt("menu_isPossible");
+                int fetchedTypeId = rs.getInt("type_id");
+
+                MenuVO menuVo = new MenuVO(menuId, menuName, menuPrice, menuIsPossible, fetchedTypeId);
+                menuVos.add(menuVo);
+            }
+        }
+
+        return menuVos;
+    }
 }
